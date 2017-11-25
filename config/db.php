f0f5c824c2b0a729867ec5203d5914e69bc1ba44
Pedro Gomes Nogueira Carneiro <pgncarneiro@gmail.com>
2017-11-25 18:36:51 -0200

    Alterações para apontamento em produção.

--- config/db.php
+++ config/db.php
@@ -13,7 +13,7 @@ else
 {
 	return [
     	'class' => 'yii\db\Connection',
-    	'dsn' => 'mysql:host=localhost;dbname=salute',
+    	'dsn' => 'mysql:host=salute-intra.mysql.uhserver.com;dbname=salute_intra',
     	'username' => 'salute_prod',
     	'password' => 'linguica7102*',
     	'charset' => 'utf8',
